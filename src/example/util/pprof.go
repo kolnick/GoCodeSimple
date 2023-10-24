de5a674e3c710ad5f29db5ea1fee5c5f61125791
caochaojie <258920553@qq.com>
2023-10-24 23:53:46 +0800

    update

--- src/example/util/pprof.go
+++ src/example/util/pprof.go
@@ -9,28 +9,6 @@ import (
 	"time"
 )
 
-func readMemStatus() {
-
-	var ms runtime.MemStats
-
-	runtime.ReadMemStats(&ms)
-
-	log.Printf("===> Alloc:%d(Bytes) HeapIdle:%d(Bytes) HeapReleased %d(Bytes)", ms.Alloc, ms.HeapIdle, ms.HeapReleased)
-}
-
-func test() {
-	containers := make([]int, 8)
-	log.Println("===> loop begin")
-
-	for i := 0; i < 32*1000*1000; i++ {
-		containers = append(containers, i)
-		if i == 16*1000*1000 {
-			readMemStatus()
-		}
-	}
-	log.Println("===> loop end")
-}
-
 func main() {
 	go func() {
 		if err := http.ListenAndServe(":7080", nil); err != nil {

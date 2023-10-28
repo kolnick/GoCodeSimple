dc3c97abf041f6d3034ce568499c39534b0c3d35
caochaojie <258920553@qq.com>
2023-10-28 13:56:45 +0800

    update

---
+++ src/framework/example/cron_robfig/robfig_test.go
@@ -0,0 +1,19 @@
+package cron_robfig
+
+//https://cron.qqe2.com/  cron web
+
+// https://github.com/robfig/cron
+import (
+	"fmt"
+	"github.com/robfig/cron"
+	"testing"
+)
+
+func TestSecondJob(t *testing.T) {
+	c := cron.New()
+
+	c.AddFunc("* * * * * ?", func() {
+		fmt.Println("执行了定时任务")
+	})
+	c.Run()
+}
